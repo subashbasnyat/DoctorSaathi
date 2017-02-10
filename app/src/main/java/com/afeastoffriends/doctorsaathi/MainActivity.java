374b788dd8f714e815c07d11d9eb3d3cf9bb8dd6
Subash Basnet <Subash Basnet>
2017-02-11 00:06:42 +0545

    main with bhai ko project

--- app/src/main/java/com/afeastoffriends/doctorsaathi/MainActivity.java
+++ app/src/main/java/com/afeastoffriends/doctorsaathi/MainActivity.java
@@ -1,19 +1,10 @@
 package com.afeastoffriends.doctorsaathi;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -21,7 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
@@ -30,18 +21,27 @@ public class MainActivity extends AppCompatActivity
 
 
     public ImageButton imgbutton;
+    public ImageButton imgbuttonfirstaid;
 
     public void init() {
         imgbutton = (ImageButton) findViewById(R.id.imgHospital);
+        imgbuttonfirstaid = (ImageButton) findViewById(R.id.imgBloodBank);
         imgbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mappin = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(mappin);
-
+            }
+        });
+        imgbuttonfirstaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent blooding = new Intent(MainActivity.this, BloodActivity.class);
+                startActivity(blooding);
             }
         });
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
